3e2eeac5a7cba955ef9ff193a7dca693c88a526e
Ubuntu <ubuntu@ip-172-31-24-52.us-west-2.compute.internal>
2016-06-10 11:19:25 +0000

    changes for install

--- run.sh
+++ run.sh
@@ -2,24 +2,24 @@
 
 # wget/install chefDK
 wget https://packages.chef.io/stable/ubuntu/12.04/chefdk_0.14.25-1_amd64.deb
-dpkg -i chefdk_0.14.25-1_amd64.deb
+sudo dpkg -i chefdk_0.14.25-1_amd64.deb
 
 # move cookbook to /var/chef/cookbooks
-mkdir /var/chef
-mkdir /var/chef/cookbooks
-mv stelligent-site-install /var/chef/cookbooks
+sudo mkdir /var/chef
+sudo mkdir /var/chef/cookbooks
+sudo cp -r stelligent-site-install-cookbook /var/chef/cookbooks
 
 # berks install/ohai,nginx fixes
 # Normally I wouldn't be doing things this way, but I don't have time to 
 # download and correct the issues with the nginx/ohai community cookbooks.
 # This is a quick fix, and not a long term solution.
-cd /var/chef/cookbooks
-berks install
-mv ~/.berkshelf/cookbooks/nginx* ~/.berkshelf/cookbooks/nginx
-mv ~/.berkshelf/cookbooks/ohai* ~/.berkshelf/cookbooks/ohai
+cd /var/chef/cookbooks/stelligent-site-install-cookbook
+sudo berks install
+sudo mv ~/.berkshelf/cookbooks/nginx* ~/.berkshelf/cookbooks/nginx
+sudo mv ~/.berkshelf/cookbooks/ohai* ~/.berkshelf/cookbooks/ohai
 
 # run chef
-chef-solo -c /var/chef/cookbooks/stelligent-site-install/solo.rb -j /var/chef/cookbooks/stelligent-site-install/web.json
+sudo chef-solo -c /var/chef/cookbooks/stelligent-site-install-cookbook/solo.rb -j /var/chef/cookbooks/stelligent-site-install-cookbook/web.json
 
 # curl localhost:8000 for test
 curl localhost:8000
---
+++ stelligent-site-install-cookbook/CHANGELOG.md
@@ -0,0 +1,11 @@
+# stelligent-site-install CHANGELOG
+
+This file is used to list changes made in each version of the stelligent-site-install cookbook.
+
+## 0.1.0
+- [your_name] - Initial release of stelligent-site-install
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
