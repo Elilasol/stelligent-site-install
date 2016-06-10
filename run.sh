ccf59c3cce2c49dc580e3c21472519b71aa06da6
Ubuntu <ubuntu@ip-172-31-24-52.us-west-2.compute.internal>
2016-06-10 12:13:50 +0000

    Might be final fix

--- run.sh
+++ run.sh
@@ -7,14 +7,14 @@ sudo dpkg -i chefdk_0.14.25-1_amd64.deb
 # move cookbook to /var/chef/cookbooks
 sudo mkdir /var/chef
 sudo mkdir /var/chef/cookbooks
-sudo cp -r stelligent-site-install-cookbook /var/chef/cookbooks
+sudo cp -r stelligent-site-install /var/chef/cookbooks
 
 # berks install/ohai,nginx fixes
-cd /var/chef/cookbooks/stelligent-site-install-cookbook
+cd /var/chef/cookbooks/stelligent-site-install
 sudo berks vendor
 
 # run chef
-sudo chef-solo -c /var/chef/cookbooks/stelligent-site-install-cookbook/solo.rb -j /var/chef/cookbooks/stelligent-site-install-cookbook/web.json
+sudo chef-solo -c /var/chef/cookbooks/stelligent-site-install/solo.rb -j /var/chef/cookbooks/stelligent-site-install/web.json
 
 # curl localhost:8000 for test
 curl localhost:8000
